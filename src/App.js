48d9a1ed90c42a9a447d1ac2cdf6536501f79934
Andrew TOwnsley <atownsley86@gmail.com>
2021-12-19 21:15:14 -0700

    tutorial .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import './App.css';
-import React, { useReducer } from 'react';
+import { useReducer, useState } from 'react';
+const Slider = ({ onChange, min, max }) => {
+  const [value, setValue] = useState(1);
+
+  return (
+    <div>
+      {value}
+      <input 
+        type="range" 
+        min={min}
+        max={max}
+        value={value}
+        onChange= {(e) => {
+          const value = Number(e.target.value)
+          onChange(value)
+          setValue(value)
+        }}
+      />
+    </div>
+  )
+
+
+}
+
 
 const reducer = (state, action) => {
   if (action === 'increment') {
-    return state + 1
+    return {
+      count: state.count + 1,
+      step: state.step,
+    }
   } else if (action === 'decrement') {
-    return state - 1
+    return {
+      count: state.count - 1,
+      step: state.step,
+    }
   } else if (action === 'reset') {
-    return 0
+    return {
+      count: 0,
+      step: state.step,
+    }
   } else {
     throw new Error();
   }
@@ -17,11 +49,16 @@ const reducer = (state, action) => {
 function App() {
 const [count, dispatch] = useReducer(
   reducer, 
-  0
+  { count: 0, step: 1 }
 )
 
   return (
     <div className="App">
+      <Slider
+        min={1}
+        max={10}
+        onChange={() => ({})}
+      />
       <h1>{count}</h1>
       <button onClick={() => dispatch('increment')}>Plus</button>
       <button onClick={() => dispatch('decrement')}>Minus</button>
